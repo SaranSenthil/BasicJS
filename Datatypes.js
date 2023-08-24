69efbdee5a7410a8dd5694043c3aef064a639e03
SaranSenthil <nrms162@gmail.com>
2023-08-24 16:56:59 +0530

    types added

--- Datatypes.js
+++ Datatypes.js
@@ -1,1 +1,36 @@
-//Datatype  const let var
+//Datatype  
+//What type of data store in a variable  defines datatyoe
+//Variable= a label which points memory location
+//Datatype number,string,boolean,undefined
+let num=1;
+let str='name';
+let bool=true;
+let n=null;
+ console.log(num,str,bool,n);
+  // to check the type of datatype
+  console.log(typeof(n));
+
+  //datatype object
+
+  let obj =[1,2,3,4,'oliva'];
+  console.log(typeof(obj));
+  let obj1={
+
+  }
+  console.log(typeof(obj1))
+
+  //Fuction type
+  function sy()
+  {
+    console.log('hello')
+  }
+
+  console.log(typeof(sy))
+
+
+//   //Types:
+//   1.number
+//   2.string
+//   3.object
+//   4.function
+//   5.null
