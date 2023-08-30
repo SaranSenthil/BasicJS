761686169ec3f98eed22037c7c4c2ee6f9f75136
SaranSenthil <nrms162@gmail.com>
2023-08-30 13:33:40 +0530

    JSON Added

--- JSObject.js
+++ JSObject.js
@@ -20,3 +20,27 @@ console.log(JSON.parse(person));
 //we have to convert object and array depend on the need. 
 
 //XML Http Request
+//Import library first - in terminal type - npm install xhr2
+
+const XMLHttpRequest=require('xhr2');
+console.log('test')
+//XMLHttprequest is  class to provide methods
+//create an object to XMLhttp req class
+let xhr = new XMLHttpRequest();
+ xhr.open('GET', "https://restcountries.com/v3.1/all");
+ xhr.onload = function(){
+console.log(xhr.responseText)
+let data = JSON.parse(xhr.responseText);
+for(let i=0; i<data.length;i++)
+{
+    console.log(data[i].flags);
+}
+ };
+ xhr.send();
+//Api req url - type browser and enter
+
+//steps for API call - data mani[pulation]
+//1.import xml library
+//2..Object creataion
+//3.connection open
+//4.load and send 
